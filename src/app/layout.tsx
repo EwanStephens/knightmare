a5fa7f9fa3be5c44d8764b18275d8ed07d3d3200
Tridevi Chakma <tridevi@gmail.com>
2025-05-16 11:24:54 +0100

    Go back to using the original icons but get it to work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,25 +31,8 @@ export default function RootLayout({
       <head>
         <link 
           rel="stylesheet" 
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" 
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=block" 
         />
-        <style>{`
-          .material-symbols-outlined {
-            font-family: 'Material Symbols Outlined';
-            font-weight: normal;
-            font-style: normal;
-            font-size: 24px;
-            line-height: 1;
-            letter-spacing: normal;
-            text-transform: none;
-            display: inline-block;
-            white-space: nowrap;
-            word-wrap: normal;
-            direction: ltr;
-            -webkit-font-feature-settings: 'liga';
-            -webkit-font-smoothing: antialiased;
-          }
-        `}</style>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased pt-16 bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-200`}
