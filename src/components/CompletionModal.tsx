68e4a06431a8acc8758ed0c46b659b0c4ea82a22
Tridevi Chakma <tridevi@gmail.com>
2025-05-22 14:28:45 +0100

    Make play again button green

--- src/components/CompletionModal.tsx
+++ src/components/CompletionModal.tsx
@@ -66,7 +66,7 @@ export default function CompletionModal({
           )}
           {allowReplays && (
             <button
-              className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition-colors duration-200"
+              className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors duration-200"
               onClick={handleReplay}
             >
               Play Again
