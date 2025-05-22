e18e261656e3d419ed9104c81b89c924b32a2929
Tridevi Chakma <tridevi@gmail.com>
2025-05-22 18:35:39 +0100

    Update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,10 @@ export default function RootLayout({
           rel="stylesheet" 
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=block" 
         />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/x-icon" href="/favicons/favicon.ico" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased pt-16`}>
         <TopBar />
